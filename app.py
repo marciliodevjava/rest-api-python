5f275f09c9df0c68d5ab37cdd8aa215f984fc6ee
marcsilva <marciliodevjava@gmail.com>
2024-01-22 20:29:24 -0300

    update app

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from resources.hotel_resource import Hoteis, Hotel
+from models.hotel_model import session
 
 app = Flask(__name__)
-
 api = Api(app)
-
-
-from resources.hotel_resource import Hoteis, Hotel
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
