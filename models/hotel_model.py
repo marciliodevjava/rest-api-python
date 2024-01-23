e815f8a85ee95ecf73f638729cb5b6c5c8337c84
marcsilva <marciliodevjava@gmail.com>
2024-01-23 09:16:35 -0300

    update hotel_model

--- models/hotel_model.py
+++ models/hotel_model.py
@@ -30,7 +30,7 @@ class HotelModel(Base):
         }
 
 # Configurar a conexão com o banco de dados SQLite
-engine = create_engine('sqlite:///banco.db')
+engine = create_engine('sqlite:///mydatabase.db', connect_args={'check_same_thread': False})
 Base.metadata.create_all(engine)
 
 # Configurar a sessão do SQLAlchemy
