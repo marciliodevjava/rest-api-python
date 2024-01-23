388073d8841409715c30666d0f77531e17c8bb2c
marcsilva <marciliodevjava@gmail.com>
2024-01-22 21:01:29 -0300

    update hotel_model

--- models/hotel_model.py
+++ models/hotel_model.py
@@ -7,11 +7,27 @@ Base = declarative_base()
 
 class HotelModel(Base):
     __tablename__ = 'hoteis'
-    hotel_id = Column(String, primary_key=True)
+    hotel_id = Column(String, primary_key=True)  # Removido autoincrement=True
     nome = Column(String)
     estrelas = Column(Float)
     diaria = Column(Float)
     cidade = Column(String)
+
+    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
+        self.hotel_id = hotel_id  # Modificado para usar o nome da coluna diretamente
+        self.nome = nome
+        self.estrelas = estrelas
+        self.diaria = diaria
+        self.cidade = cidade
+
+    def json(self):
+        return {
+            'hotel_id': str(self.hotel_id),
+            'nome': str(self.nome),
+            'estrelas': float(self.estrelas),
+            'diaria': float(self.diaria),
+            'cidade': str(self.cidade)
+        }
 
 # Configurar a conexão com o banco de dados SQLite
 engine = create_engine('sqlite:///banco.db')
