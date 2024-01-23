f2ae3431d07a5875e1d76bcd2f9ab9b8a0e463ff
marcsilva <marciliodevjava@gmail.com>
2024-01-23 10:07:21 -0300

    update sql_alchemy.py

---
+++ sql_alchemy.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+# Configurar a conexão com o banco de dados SQLite
+engine = create_engine('sqlite:///mydatabase.db', connect_args={'check_same_thread': False})
+Base.metadata.create_all(engine)
+
+# Configurar a sessão do SQLAlchemy
+Session = sessionmaker(bind=engine)
+session = Session()
