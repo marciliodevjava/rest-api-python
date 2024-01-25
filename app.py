dcd4a0b29919b2a3cf7adc1cc3667b39e4e2dd58
marciliodevjava <marciliodevjava@gmail.com>
2024-01-25 19:19:50 -0300

    Update app

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.hotel_resource import Hotel, Hoteis
+from resources.usuario_resource import Usuario, Usuarios, UsuarioRegistro
+from sql_alchemy import Base
+from sql_alchemy import engine
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(Usuarios, '/usuarios')
+api.add_resource(Usuario, '/usuario/<int:user_id>')
+api.add_resource(UsuarioRegistro, '/cadastro')
 
 if __name__ == '__main__':
+    Base.metadata.create_all(engine)
     app.run(debug=False)
