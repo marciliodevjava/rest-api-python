19d70dfe8cafa5041b4f45c2a82fab6cba86fc24
marciliodevjava <marciliodevjava@gmail.com>
2024-01-25 16:24:35 -0300

    Update hotel_resource.py

--- resources/hotel_resource.py
+++ resources/hotel_resource.py
@@ -38,10 +38,10 @@ class Hoteis(Resource):
 class Hotel(Resource):
     def __init__(self):
         self.__parser = reqparse.RequestParser()
-        self.__parser.add_argument('nome', type=str)
-        self.__parser.add_argument('estrelas', type=float)
-        self.__parser.add_argument('diaria', type=float)
-        self.__parser.add_argument('cidade', type=str)
+        self.__parser.add_argument('nome', type=str, required=True, help="O campo 'nome' tem que ser enviado")
+        self.__parser.add_argument('estrelas', type=float, required=True, help="O campo 'estrelas' tem que ser enviado")
+        self.__parser.add_argument('diaria', type=float, required=True, help="O campo 'diaria' tem que ser enviado")
+        self.__parser.add_argument('cidade', type=str, required=True, help="O campo 'cidade' tem que ser enviado")
 
     def get(self, hotel_id):
         hotel = session.query(HotelModel).filter_by(hotel_id=hotel_id).first()
@@ -53,17 +53,19 @@ class Hotel(Resource):
         dados = self.__parser.parse_args()
 
         if not hotel_id:
-            return {'mensagem': 'O campo hotel_id não pode ser nulo'}, 400
+            return {'message': 'O campo hotel_id não pode ser nulo'}, 400
 
         hotel_objeto = HotelModel(hotel_id, **dados)
         hotel = HotelModel.busca_hotel(hotel_id)
         if hotel:
-            return {'mensagem': 'Hotel já existe na base de dados', 'hotel': hotel.json()}, 200
+            return {'message': 'Hotel já existe na base de dados', 'hotel': hotel.json()}, 200
 
-        session.add(hotel_objeto)
-        session.commit()
-
-        return {'mensagem': 'Hotel adicionado com sucesso', 'hotel': hotel_objeto.json()}, 201
+        try:
+            session.add(hotel_objeto)
+            session.commit()
+        except:
+            return {'message': 'Occoreu um erro ao gravar informação'}, 500
+        return {'message': 'Hotel adicionado com sucesso', 'hotel': hotel_objeto.json()}, 201
 
     def put(self, hotel_id):
         dados = self.__parser.parse_args()
@@ -76,17 +78,26 @@ class Hotel(Resource):
             hotel.estrelas = dados['estrelas']
             hotel.diaria = dados['diaria']
             hotel.cidade = dados['cidade']
-            session.commit()
+            try:
+                session.commit()
+            except:
+                return {'message': 'Occoreu um erro ao gravar informação'}, 500
             return {'hotel': hotel_objeto.json()}, 200
         else:
-            session.add(hotel_objeto)
-            session.commit()
+            try:
+                session.add(hotel_objeto)
+                session.commit()
+            except:
+                return {'message': 'Occoreu um erro ao gravar informação'}, 500
             return {'hotel': hotel_objeto.json()}, 201
 
     def delete(self, hotel_id):
         hotel = HotelModel.busca_hotel(hotel_id)
         if hotel:
-            session.delete(hotel)
+            try:
+                session.delete(hotel)
+            except:
+                return {'message': 'Occoreu um erro ao deletar a informação'}, 500
             return {'mensagem': 'Hotel removido com sucesso'}
 
         return {'mensagem': 'Hotel not found'}, 404
