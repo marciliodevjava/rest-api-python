0658711607a7378e6351ca8d7311968fb440ef88
marciliodevjava <marciliodevjava@gmail.com>
2024-01-25 19:20:32 -0300

    Update usuario_resource

--- resources/usuario_resource.py
+++ resources/usuario_resource.py
@@ -4,13 +4,15 @@ from models.usuario_model import UsuarioModel
 from sql_alchemy import session
 
 
+# /usuarios
 class Usuarios(Resource):
     def get(self):
-        usuario = session.query(UsuarioModel).filter_by(UsuarioModel.nome).all()
+        usuario = session.query(UsuarioModel).order_by(UsuarioModel.nome).all()
         usuarios = [usuarios.json() for usuarios in usuario]
         return {'Usuários': usuarios}
 
 
+# /usuario/<int:user_id>
 class Usuario(Resource):
     def __init__(self):
         self.__parcer = reqparse.RequestParser()
@@ -21,7 +23,7 @@ class Usuario(Resource):
     def get(self, user_id):
         usuario = session.query(UsuarioModel).filter_by(user_id=user_id).first()
         if usuario:
-            return {'Usuário': usuario}
+            return {'Usuário': usuario.json()}
         return {'message': 'User not found'}, 404
 
     def delete(self, user_id):
@@ -33,3 +35,36 @@ class Usuario(Resource):
                 return {'message': 'Ocoreu um erro ao deletar o usuário'}, 500
             return {'message': f'Usuário {usuario.nome} deletado com sucesso!'}
         return {'message': 'Usuario not found'}, 404
+
+
+class UsuarioRegistro(Resource):
+    def __init__(self):
+        self.__parcer = reqparse.RequestParser()
+        self.__parcer.add_argument('nome', type=str, required=None)
+        self.__parcer.add_argument('login', type=str, required=True, help='O campo login tem que ser passado.')
+        self.__parcer.add_argument('senha', type=str, required='O campo senha tem que ser passado.')
+
+    # /cadastro
+    def post(self):
+        dados = self.__parcer.parse_args()
+        login = session.query(UsuarioModel).filter_by(login=dados['login']).first()
+        if login:
+            return {'message': f'Usuário com o login {dados["login"]} já existe'}, 500
+        user = UsuarioModel(**dados)
+        try:
+            session.add(user)
+            session.commit()
+        except:
+            return {'message': 'Ocorreu um erro para salvar o usuário'}
+        return {'message': 'USUARIO CRIADO COM SUCESSO!',
+                'Usuário': user.json()}, 201
+
+
+class UsuarioLogin(Resource):
+    def __init__(self):
+        self.__parcer = reqparse.RequestParser()
+        self.__parcer.add_argument('login', type=str, required=True, help='O campo login não foi enviado')
+        self.__parcer.add_argument('senha', type=str, required=True, help='O campo senha não foi envaido')
+
+    def post(self):
+        pass
