0e0c266d3835deda9152ab138ffe742c4726f4da
marciliodevjava <marciliodevjava@gmail.com>
2024-01-25 19:20:25 -0300

    Update usuario_model.py

--- models/usuario_model.py
+++ models/usuario_model.py
@@ -1,32 +1,28 @@
-import datetime
-
-from sqlalchemy import Column, String, Integer
-
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.sql import func
 from sql_alchemy import Base, session
 
-
 class UsuarioModel(Base):
-    _tablename__ = 'usuarios'
+    __tablename__ = 'usuarios'
     user_id = Column(Integer, primary_key=True)
     nome = Column(String(150))
     login = Column(String(40))
     senha = Column(String(100))
+    data = Column(DateTime(timezone=True), server_default=func.now())
 
     def __init__(self, nome, login, senha):
-        self.__nome = nome
-        self.__login = login
-        self.__senha = senha
+        self.nome = nome
+        self.login = login
+        self.senha = senha
 
     def json(self):
         return {
-            'nome': self.__nome,
+            'nome': self.nome,
             'login': self.login,
-            'data': datetime.datetime.now()
+            'data': self.data.strftime("%Y-%m-%d %H:%M:%S")
         }
 
     @classmethod
-    def busca_usuario(cls, nome):
-        usuario = session.query(cls).filter_by(nome=nome).first()
-        if usuario:
-            return usuario
-        return None
+    def busca_usuario(cls, user_id):
+        usuario = session.query(cls).filter_by(user_id=user_id).first()
+        return usuario
