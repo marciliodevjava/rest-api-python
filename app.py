006fe4720425ebd0202293b20fc8201140517c8f
marciliodevjava <marciliodevjava@gmail.com>
2024-01-26 19:25:15 -0300

    Update app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from my_custom_json_encoder import CustomJSONEncoder
 from flask import Flask
-from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_restful import Api
+
+from my_custom_json_encoder import CustomJSONEncoder
 from resources.hotel_resource import Hotel, Hoteis
 from resources.usuario_resource import Usuario, Usuarios, UsuarioRegistro, UsuarioLogin
 from sql_alchemy import Base, engine
