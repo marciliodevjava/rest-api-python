4ad8fd9eb54b17cd31b7faaba2b6e4e83d1495c3
marciliodevjava <marciliodevjava@gmail.com>
2024-01-27 22:43:52 -0300

    Update message

--- enuns/message.py
+++ enuns/message.py
@@ -1,6 +1,24 @@
 from enuns import Enum
 
-class MessagensEnum(Enum):
+
+class MessagensEnumUsuario(Enum):
     ERRO_DELECAO_USUARIO = "Ocorreu um erro ao deletar o usuário {}."
     ERRO_USUARIO_NOT_FOUND = 'Usuario not found'
     USUARIO_DELETADO_COM_SUCESSO = 'Usuário {} deletado com sucesso!'
+    OCORREU_UM_ERRO_AO_SALVAR_USUARIO = 'Ocorreu um erro para salvar o usuário'
+    USUARIO_CRIADO_COM_SUCESSO = 'Ocorreu um erro para salvar o usuário'
+    USUARIO_COM_LOGIN_JA_EXISTE = 'Usuário com o login {} já existe'
+    USUARIO_LOGADO_COM_SUCESSO = 'Usuario Logado com SUCESSO!'
+    SENHA_INCORRETA = 'Senha INCORRETA, tente novamente'
+    USUARIO_E_SENHA_INCORRETO = 'Usuario e senha incoretos!'
+    HOTEL_NOT_FOUND = 'Hotel not found.'
+
+
+class MessagensEnumHotel(Enum):
+    HOTEL_NOT_FOUND = 'Hotel not found.'
+    HOTEL_COM_NAO_PODE_NULO = 'O campo hotel_id não pode ser nulo'
+    HOTEL_EXISTE_NA_BASE_DE_DADO = 'Hotel já existe na base de dados'
+    HOTEL_OCOREU_ERRO_GRAVAR_INFORMACAO = 'Ocoreu um erro ao gravar informação'
+    HOTEL_ADICIONADO_COM_SUCESSO = 'Hotel adicionado com sucesso'
+    HOTEL_ERRO_DELETAR_INFORMACAO = 'Occoreu um erro ao deletar a informação'
+    HOTEL_REMOVIDO_COM_SUCESSO = 'Hotel removido com sucesso'
