d269c27ff981fd209e321fd061e24a62c6584ddd
marciliodevjava <marciliodevjava@gmail.com>
2024-01-27 21:38:51 -0300

    Create message

---
+++ enuns/message.py
@@ -0,0 +1,6 @@
+from enuns import Enum
+
+class MessagensEnum(Enum):
+    ERRO_DELECAO_USUARIO = "Ocorreu um erro ao deletar o usuário {}."
+    ERRO_USUARIO_NOT_FOUND = 'Usuario not found'
+    USUARIO_DELETADO_COM_SUCESSO = 'Usuário {} deletado com sucesso!'
