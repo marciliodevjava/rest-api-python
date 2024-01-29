dadd4c44c01cb46cfbdfb6a0329cf9786aa0d529
marciliodevjava <marciliodevjava@gmail.com>
2024-01-28 23:47:55 -0300

    Create filtros

--- filtros/filtros.py
+++ filtros/filtros.py
@@ -5,3 +5,15 @@ class FiltroHotel:
             return {
                 'cidade': cidade
             }
+
+    @staticmethod
+    def normalize_filter_offset(offset=None, **dados):
+        if offset:
+            return offset
+        return 0
+
+    @staticmethod
+    def normalize_filter_limit(limit=None, **dado):
+        if limit:
+            return limit
+        return 10
