09ff81c5836c7c6a1aa640e62d87ddfadaf68c7b
marciliodevjava <marciliodevjava@gmail.com>
2024-01-29 14:55:48 -0300

    Update message

--- enuns/message.py
+++ enuns/message.py
@@ -12,6 +12,9 @@ class MessagensEnumUsuario(Enum):
     SENHA_INCORRETA = 'Senha INCORRETA, tente novamente'
     USUARIO_E_SENHA_INCORRETO = 'Usuario e senha incoretos!'
     HOTEL_NOT_FOUND = 'Hotel not found.'
+    MENSAGEM_PARANS_LOGIN = 'O campo login tem que ser passado.'
+    MENSAGEM_PARANS_SENHA = 'O campo senha tem que ser passado.'
+    TOKEN_REVOGADO = "Token has been revoked"
 
 
 class MessagensEnumHotel(Enum):
@@ -22,7 +25,14 @@ class MessagensEnumHotel(Enum):
     HOTEL_ADICIONADO_COM_SUCESSO = 'Hotel adicionado com sucesso.'
     HOTEL_ERRO_DELETAR_INFORMACAO = 'Occoreu um erro ao deletar a informação.'
     HOTEL_REMOVIDO_COM_SUCESSO = 'Hotel removido com sucesso.'
-    HOTEL_SOLICITACAO_SEM_CONTEUDO ='Solicitação não retornou conteúdo.'
+    HOTEL_SOLICITACAO_SEM_CONTEUDO = 'Solicitação não retornou conteúdo.'
+    MENSAGEM_PARANS_NOME = "O campo 'nome' tem que ser enviado"
+    MENSAGEM_PARANS_ESTRELAS = "O campo 'estrelas' tem que ser enviado"
+    MENSAGEM_PARANS_DIARIA = "O campo 'diaria' tem que ser enviado"
+    MENSAGEM_PARANS_CIDADE = "O campo 'cidade' tem que ser enviado"
 
-class MessagensSites(Enum):
+
+class MessagensEnumSites(Enum):
     SITES_NAO_ENCONTRATOS = 'Não existe nenhum site na base de dados'
+    MENSAGEM_PARANS_URL = "O Campo 'url' tem que ser enviado"
+    MENSAGEM_PARANS_NOME = "O Campo 'nome tem que ser enviado"
