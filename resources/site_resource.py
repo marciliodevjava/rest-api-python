3cbe5ddd97a4899fe419a57dc2ac236b59059792
marciliodevjava <marciliodevjava@gmail.com>
2024-01-29 14:55:58 -0300

    Update site_resource.py

--- resources/site_resource.py
+++ resources/site_resource.py
@@ -1,6 +1,6 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
-from enuns.message import MessagensSites
+from enuns.message import MessagensEnumSites
 from models.sites import SitesModel
 from sql_alchemy import session
 
@@ -11,4 +11,17 @@ class Sites(Resource):
         site = [site.json() for site in sites]
         if sites:
             return {'Sites': site}
-        return {'message': MessagensSites.SITES_NAO_ENCONTRATOS}
+        return {'message': MessagensEnumSites.SITES_NAO_ENCONTRATOS}
+
+
+class Site(Resource):
+    def __init__(self):
+        self.__sites_parans = reqparse.RequestParser()
+        self.__sites_parans.add_argument('url', type=str, required=True, help=MessagensEnumSites.MENSAGEM_PARANS_URL)
+        self.__sites_parans.add_argument('nome', type=str, required=True, help=MessagensEnumSites.MENSAGEM_PARANS_NOME)
+
+    def get(self, url):
+        pass
+
+    def post(self, url):
+        pass
