1de635ca7fad27f054754ed7013a18e73adf94c2
marciliodevjava <marciliodevjava@gmail.com>
2024-01-29 15:45:29 -0300

    Update site_resource.py

--- resources/site_resource.py
+++ resources/site_resource.py
@@ -25,26 +25,30 @@ class Site(Resource):
         site = SitesModel.buscar_site_url(url)
         if site:
             return {'Site': site.json()}, 200
-        return {'message': MessagensEnumSites.SITES_NAO_ENCONTRATOS}
+        return {'message': MessagensEnumSites.SITES_NAO_ENCONTRATOS}, 404
 
     def post(self):
         dados = self.__sites_parans.parse_args()
         site = SiteMapper.mapear_site(dados)
         if site:
-            try:
-                site = SitesModel(site)
-                session.add(site)
-                session.commit()
-                return {'Site': site.json()}
-            except:
-                return {'message': MessagensEnumSites.SITE_ERRO_AO_SALVAR_SITE}
-        return {'message': MessagensEnumSites.SITE_ERRO_ENVIAR_PARAMETROS}
+            buscar_site = SitesModel.buscar_site_url(site.get('site_url'))
+            if not buscar_site:
+                try:
+                    site = SitesModel(site)
+                    session.add(site)
+                    session.commit()
+                    return {'Site': site.json()}, 201
+                except:
+                    return {'message': MessagensEnumSites.SITE_ERRO_AO_SALVAR_SITE}, 500
+            else:
+                return {'message': MessagensEnumSites.SITE_JÁ_EXISTE_NA_BASE_DE_DADOS}, 200
+        return {'message': MessagensEnumSites.SITE_ERRO_ENVIAR_PARAMETROS}, 500
 
     def delete(self, site_id):
         site = SitesModel.buscar_site_id(site_id)
         if site:
             result = SitesModel.deletar_site_id(site)
             if result:
-                return {'message': MessagensEnumSites.SITE_DELETADO_COM_SUCESSO}
-            return {'message': MessagensEnumSites.SITE_OCORREU_UM_ERRO_DELETAR}
-        return {'message': MessagensEnumSites.SITE_NÃO_ENCONTRATO_PARA_DELETAR}
+                return {'message': MessagensEnumSites.SITE_DELETADO_COM_SUCESSO}, 200
+            return {'message': MessagensEnumSites.SITE_OCORREU_UM_ERRO_DELETAR}, 500
+        return {'message': MessagensEnumSites.SITE_NÃO_ENCONTRATO_PARA_DELETAR}, 404
