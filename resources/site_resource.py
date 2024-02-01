22505b8fac609b4b73aa065c07eaa3a1e9200405
marciliodevjava <marciliodevjava@gmail.com>
2024-01-31 21:59:40 -0300

    Update site_resource.py

--- resources/site_resource.py
+++ resources/site_resource.py
@@ -2,7 +2,7 @@ from flask_restful import Resource, reqparse
 
 from enuns.message import MessagensEnumSites
 from mapper.site_mapper import SiteMapper
-from models.sites import SitesModel
+from models.sites_model import SitesModel
 from sql_alchemy import session
 
 
@@ -22,27 +22,31 @@ class Site(Resource):
         self.__sites_parans.add_argument('nome', type=str, required=True, help=MessagensEnumSites.MENSAGEM_PARANS_NOME)
 
     def get(self, url):
-        site = SitesModel.buscar_site_url(url)
-        if site:
-            return {'Site': site.json()}, 200
-        return {'message': MessagensEnumSites.SITES_NAO_ENCONTRATOS}, 404
+        try:
+            site = SitesModel.buscar_site_url(url)
+            if site:
+                return {'Site': site.json()}, 200
+            return {'message': MessagensEnumSites.SITES_NAO_ENCONTRATOS}, 404
+        except:
+            return {'message': MessagensEnumSites.ERRO_AO_CONSULTAR_NO_BANCO}, 500
 
     def post(self):
-        dados = self.__sites_parans.parse_args()
-        site = SiteMapper.mapear_site(dados)
-        if site:
-            buscar_site = SitesModel.buscar_site_url(site['site_url'])
-            if not buscar_site:
-                try:
-                    site = SitesModel(site)
-                    session.add(site)
-                    session.commit()
-                    return {'Site': site.json()}, 201
-                except:
-                    return {'message': MessagensEnumSites.SITE_ERRO_AO_SALVAR_SITE}, 500
-            else:
-                return {'message': MessagensEnumSites.SITE_JÁ_EXISTE_NA_BASE_DE_DADOS}, 200
-        return {'message': MessagensEnumSites.SITE_ERRO_ENVIAR_PARAMETROS}, 500
+        try:
+            dados = self.__sites_parans.parse_args()
+            site = SiteMapper.mapear_site(dados)
+            if site:
+                buscar_site = SitesModel.buscar_site_url(site['site_url'])
+                if not buscar_site:
+                    try:
+                        site = SitesModel(site)
+                        session.add(site)
+                        session.commit()
+                        return {'Site': site.json()}, 201
+                    except:
+                        return {'message': MessagensEnumSites.SITE_ERRO_AO_SALVAR_SITE}, 500
+                return {'message': MessagensEnumSites.SITE_JÁ_EXISTE_NA_BASE_DE_DADOS}, 400
+        except:
+            return {'message': MessagensEnumSites.SITE_ERRO_ENVIAR_PARAMETROS}, 500
 
     def delete(self, site_id):
         site = SitesModel.buscar_site_id(site_id)
