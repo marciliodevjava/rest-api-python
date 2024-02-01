f66c27c8cda7e73705fd1adffd46eb45f83ccc44
marciliodevjava <marciliodevjava@gmail.com>
2024-01-31 22:44:37 -0300

    Update cria_usuario

--- cria_usuario.py
+++ cria_usuario.py
@@ -5,7 +5,6 @@ cursor = connection.cursor()
 
 cria_tabela = "CREATE TABLE IF NOT EXISTS usuario(user_id INTEGER Primary KEY NOT NULL, nome VARCHAR(100) NOT NULL, login VARCHAR(100) NOT NULL, senha VARCHAR(100) NOT NULL);"
 
-
 cursor.execute(cria_tabela)
 connection.commit()
 connection.close()
